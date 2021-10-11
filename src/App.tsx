b600105d49850fa817ac389f5bea5094221b2af6
geallenboy <gejialun88@163.com>
2021-10-11 15:45:59 +0800

    自动修复

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.tsx</code> and save to reload.</p><a className="App-link" href="https://reactjs.org"
           target="_blank" rel="noopener noreferrer" >
-          Learn React99
+          Learn React9944
         </a>
       </header>
     </div>
